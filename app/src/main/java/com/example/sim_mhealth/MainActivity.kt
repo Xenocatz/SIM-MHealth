9c04874cef879511bbaed786ec76795e209e0191
M.Faiz <luthfimuladi24@gmail.com>
2025-12-17 17:50:00 +0700

    fix ui: hide system bar (fullscreen)

--- app/src/main/java/com/example/sim_mhealth/MainActivity.kt
+++ app/src/main/java/com/example/sim_mhealth/MainActivity.kt
@@ -3,24 +3,31 @@ package com.example.sim_mhealth
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.sim_mhealth.ui.auth.LoginScreen
-import com.example.sim_mhealth.ui.auth.RegisterScreen
-import com.example.sim_mhealth.ui.introScreen.IntroScreen
-import com.example.sim_mhealth.ui.theme.SIMMHealthTheme
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.example.sim_mhealth.ui.navigation.AppNavigation
+import com.example.sim_mhealth.ui.theme.SIMMHealthTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             SIMMHealthTheme {
+                hideSystemBars()
                 AppNavigation()
             }
         }
+    }
+
+    private fun hideSystemBars() {
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
     }
 }
